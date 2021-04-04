abc717e732c1a540b0531eeed08c80b71631627d
lucasgueller <lucasgueller@hotmail.com>
2021-04-04 11:46:20 -0300

    Card - last part

--- curso-react-cod3r/fundamentos-react/src/App.jsx
+++ curso-react-cod3r/fundamentos-react/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react"
+import './App.css'
 
 import Card from './components/Layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
@@ -8,21 +9,24 @@ import ComParametro from './components/basicos/ComParametro'
 import Primeiro from './components/basicos/Primeiro'
 
 export default () =>
-    <div id="app">
+    <div className="App">
         <h1><strong> Fundamentos React </strong></h1>
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio min={1} max={60} />
-        </Card>
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
-        <Card titulo="#02 - Com Parâmetro">
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro"
-                nota={5.0} />
-        </Card>
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro />
-        </Card>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório" color=" #2ab7ca">
+                <Aleatorio min={1} max={60} />
+            </Card>
+            <Card titulo="#03 - Fragmento" color="#7bc043">
+                <Fragmento />
+            </Card>
+            <Card titulo="#02 - Com Parâmetro" color="#ee4035 ">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro"
+                    nota={5.0} />
+            </Card>
+            <Card titulo="#01 - Primeiro Componente" color="#fe8a71">
+                <Primeiro />
+            </Card>
+        </div>
+
     </div >
