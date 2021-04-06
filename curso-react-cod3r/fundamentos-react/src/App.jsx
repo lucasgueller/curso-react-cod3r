ffb477149fd0b39d21bd148d56f60687cea75c3f
lucasgueller <lucasgueller@hotmail.com>
2021-04-06 11:04:18 -0300

    using props.children

--- curso-react-cod3r/fundamentos-react/src/App.jsx
+++ curso-react-cod3r/fundamentos-react/src/App.jsx
@@ -2,6 +2,8 @@
 import React from "react"
 import './App.css'
 
+import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 import Card from './components/Layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
@@ -12,10 +14,17 @@ export default () =>
     <div className="App">
         <h1><strong> Fundamentos React </strong></h1>
         <div className="Cards">
+            <Card titulo="#05 - Componente com Filhos" color=" #36454f">
+                <Familia sobrenome='Ferreira'>
+                    <FamiliaMembro nome='Pedro' />
+                    <FamiliaMembro nome='Ana' />
+                    <FamiliaMembro nome='Gustavo' />
+                </ Familia>
+            </Card>
             <Card titulo="#04 - Desafio Aleatório" color=" #2ab7ca">
                 <Aleatorio min={1} max={60} />
             </Card>
-            <Card titulo="#03 - Fragmento teste" color="#7bc043">
+            <Card titulo="#03 - Fragmento" color="#7bc043">
                 <Fragmento />
             </Card>
             <Card titulo="#02 - Com Parâmetro" color="#ee4035 ">
---
+++ curso-react-cod3r/fundamentos-react/src/components/basicos/FamiliaMembro.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    return (
+        <div>{props.nome} {props.sobrenome} </div>
+    )
+}
